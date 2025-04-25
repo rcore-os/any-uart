2c670e9b3074d533eceed1ca16a18bbdc32d35e6
周睿 <zrufo747@outlook.com>
2025-04-25 17:23:20 +0800

    fmt

--- any-uart/src/ns16550.rs
+++ any-uart/src/ns16550.rs
@@ -15,7 +15,7 @@ impl Ns16550 {
                     x86_64::instructions::port::Port::<u8>::new((uart.base + reg) as _)
                         .write(val as _);
                     #[cfg(target_arch = "aarch64")]
-                    todo!()
+                    todo!();
                 }
                 IoKind::Mmio16 => {
                     uart.reg::<u16>(reg).write_volatile(val as _);
@@ -35,9 +35,10 @@ impl Ns16550 {
             match uart.io_kind {
                 IoKind::Port => {
                     #[cfg(target_arch = "x86_64")]
-                    x86_64::instructions::port::Port::<u8>::new((uart.base + reg) as _).read() as u32
+                    return x86_64::instructions::port::Port::<u8>::new((uart.base + reg) as _)
+                        .read() as u32;
                     #[cfg(target_arch = "aarch64")]
-                    0
+                    return 0;
                 }
                 IoKind::Mmio16 => uart.reg::<u16>(reg).read_volatile() as _,
                 IoKind::Mmio32 | IoKind::Mmio => uart.reg::<u32>(reg).read_volatile(),
